d74d5eaaaf7bfe152b256f4a9c9bd09f9453653c
Richard Freitas <wesleyrichard30@gmail.com>
2025-02-27 16:33:48 -0300

    feat: add all history page and styles with a list of cycles

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -1,3 +1,51 @@
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale/pt-BR';
+import { UseCycles } from '../../hooks/use-cycles';
+import { HistoryContainer, HistoryList, Status } from './styles';
+
 export function History() {
-  return <main></main>;
+  const { cycles } = UseCycles();
+
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {cycles.map((cycle) => (
+              <tr key={cycle.id}>
+                <td>{cycle.task}</td>
+                <td>{cycle.minutesAmount} minutos</td>
+                <td>
+                  {formatDistanceToNow(new Date(cycle.startDate), {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                </td>
+                <td>
+                  {cycle.finishedDate && <Status $statusColor="green">Concluído</Status>}
+                  {cycle.interruptedDate && (
+                    <Status $statusColor="red">Interrompido</Status>
+                  )}
+                  {!cycle.finishedDate && !cycle.interruptedDate && (
+                    <Status $statusColor="yellow">Em andamento</Status>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
 }
